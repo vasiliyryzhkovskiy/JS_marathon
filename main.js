931bfaf68c8546bc6ea03a2d89ce3ec0b56e24eb
vasiliyryzhkovskiy <september86@mail.ru>
2020-09-24 00:45:04 +0300

    HomeWork 2

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-alert("Hello Marathon");
+const firstRow = "мама мыла раму";
+const secondRow = "собака друг человека";
 
-console.log("CONSOLE - Hello Marathon");
+console.log(getRow(firstRow, secondRow));
 
+function getRow(firstRow, secondRow) {
+  const charCode = 1072;
+  return getCountCharCodeInRow(charCode, firstRow) >
+    getCountCharCodeInRow(charCode, secondRow)
+    ? firstRow
+    : secondRow;
+}
+
+function getCountCharCodeInRow(charCode, row) {
+  let count = 0;
+  for (let i = 0; i < row.length; i++) {
+    if (charCode === row.charCodeAt(i)) {
+      count = count + 1;
+    }
+  }
+  return count;
+}
